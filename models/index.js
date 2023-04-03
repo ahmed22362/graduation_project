573b84783a9fb02c83d467e4b9f8b54b0d7c5ffd
ahmed hamada <ahmedhamada496@gmail.com>
2023-04-03 15:17:16 +0200

    replace local db connection with cloud db

--- models/index.js
+++ models/index.js
@@ -18,19 +18,19 @@ const UserIssue = require("./joinTables/user_issue")
 const IssueEmployee = require("./joinTables/issue_employee")
 const catchAsync = require("../utils/catchAsync")
 // Connect with the local databases
-const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
-  host: config.HOST,
-  dialect: config.dialect,
-  pool: {
-    max: config.pool.max,
-    min: config.pool.min,
-  },
-  logging: false,
-})
-// Connect to elephantSql server for postgres
-// const sequelize = new Sequelize(config.ElephantURL, {
+// const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+//   host: config.HOST,
+//   dialect: config.dialect,
+//   pool: {
+//     max: config.pool.max,
+//     min: config.pool.min,
+//   },
 //   logging: false,
 // })
+// Connect to elephantSql server for postgres
+const sequelize = new Sequelize(config.ElephantURL, {
+  logging: false,
+})
 
 // Define Database
 const db = {}
